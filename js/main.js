3169336bde0342359370d80bca985dbd294a7cf0
miletskiy <s.miletskiy@gmail.com>
2017-02-11 18:05:33 +0200

    colingz:header and gallery

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@ $(document).ready(function() {
     $("#slider").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 3000,
         infinite: true,
         cssEase: 'linear',
         arrows: false,
+        prevArrow: '<button type="button" class="slick-prev slick-arrow">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next slick-arrow">Next</button>',
     });
 
 });
