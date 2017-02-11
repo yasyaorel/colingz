858957a7912d1449bc2056d10d1b392cf1490dba
miletskiy <s.miletskiy@gmail.com>
2017-02-11 18:29:52 +0200

    colingz:add arrows to slick slider

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ $(document).ready(function() {
         autoplaySpeed: 3000,
         infinite: true,
         cssEase: 'linear',
-        arrows: false,
-        prevArrow: '<button type="button" class="slick-prev slick-arrow">Previous</button>',
-        nextArrow: '<button type="button" class="slick-next slick-arrow">Next</button>',
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev slick-arrow-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next slick-arrow-next"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>',
     });
 
 });
