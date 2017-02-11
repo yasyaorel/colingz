9142cb21003e3e7101a9429ccd11ed2b0414fac8
miletskiy <s.miletskiy@gmail.com>
2017-02-11 11:32:14 +0200

    colingz:initial

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Slick slider
+
+
+$(document).ready(function() {
+
+    $("#slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        cssEase: 'linear',
+        arrows: false,
+    });
+
+});
